e746796394efca2e800939568ce07faeb910a4e2
Apple <apple@Apples-MacBook-Pro-63.local>
2019-06-20 15:56:01 -0400

    Iteration 2

--- emojiProject/ViewController.swift
+++ emojiProject/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Based on the fruit lemon...", message: "You are sour but make many things better!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
